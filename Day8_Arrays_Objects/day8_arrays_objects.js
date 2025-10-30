f63bc7dbb526c6e5f59e8c98b0ab1d86ec2e5272
DajaT <dtrotter03@gmail.com>
2025-10-30 19:47:37 -0400

    Pushing updates for Day 8

--- Day8_Arrays_Objects/day8_arrays_objects.js
+++ Day8_Arrays_Objects/day8_arrays_objects.js
@@ -1,23 +1,25 @@
 //Arrays -- store multiple calues in a single variable
-let grocery_list = ["eggs", "almond milk", "bread", "noodles"];
+// let grocery_list = ["eggs", "almond milk", "bread", "noodles"];
 
-console.log(grocery_list[0])
-console.log(grocery_list.length)
+// console.log(grocery_list[0])
+// console.log(grocery_list.length)
 
-let testUsers = ["john doe", "jane doe", "admin user"];
-let testPasswords = ["password1", "password25", "pass10"];
+// let testUsers = ["john doe", "jane doe", "admin user"];
+// let testPasswords = ["password1", "password25", "pass10"];
 
-//push() - adding to end of array
-//unshift() - adding to the beginning of array
-//slice() - provides a subset
-//includes() - check if a value exists
+// //push() - adding to end of array
+// //unshift() - adding to the beginning of array
+// //slice() - provides a subset
+// //includes() - check if a value exists
 
-testUsers.push('new_tester');
-console.log(testUsers);
+// testUsers.push('new_tester');
+// console.log(testUsers);
 
 //Objects - store data in key-value pairs
 let card = {
-    fullName: "Michael";
-    email: "testeremail@email.com";
-    phone: 5554565678;
+    fullName: "Michael",
+    email: "testeremail@email.com",
+    phone: 5554565678,
 }
+
+console.log(card.fullName); //find name using the property in the name
