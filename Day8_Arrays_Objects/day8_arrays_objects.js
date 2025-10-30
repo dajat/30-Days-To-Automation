1f8a241e47563051a1a68c75ac5c573b328aa266
DajaT <dtrotter03@gmail.com>
2025-10-30 19:32:53 -0400

    Updating Day 8 files

--- Day8_Arrays_Objects/day8_arrays_objects.js
+++ Day8_Arrays_Objects/day8_arrays_objects.js
@@ -1,3 +1,6 @@
-let grocery_list = ["eggs", "almond milk", "bread"];
+let grocery_list = ["eggs", "almond milk", "bread", "noodles"];
 
 console.log(grocery_list[0])
+console.log(grocery_list.length)
+
+let testUsers = ["john doe", "jane doe", "admin user"];
