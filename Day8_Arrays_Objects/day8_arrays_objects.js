c6aee7e8989b8d878e6eef2497a299e83cb5f2d6
DajaT <dtrotter03@gmail.com>
2025-10-30 19:42:08 -0400

    Adding new commits for Day 8 Practice

--- Day8_Arrays_Objects/day8_arrays_objects.js
+++ Day8_Arrays_Objects/day8_arrays_objects.js
@@ -1,6 +1,23 @@
+//Arrays -- store multiple calues in a single variable
 let grocery_list = ["eggs", "almond milk", "bread", "noodles"];
 
 console.log(grocery_list[0])
 console.log(grocery_list.length)
 
 let testUsers = ["john doe", "jane doe", "admin user"];
+let testPasswords = ["password1", "password25", "pass10"];
+
+//push() - adding to end of array
+//unshift() - adding to the beginning of array
+//slice() - provides a subset
+//includes() - check if a value exists
+
+testUsers.push('new_tester');
+console.log(testUsers);
+
+//Objects - store data in key-value pairs
+let card = {
+    fullName: "Michael";
+    email: "testeremail@email.com";
+    phone: 5554565678;
+}
