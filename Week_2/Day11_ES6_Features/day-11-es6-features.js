ff477ba4fecdf11859451e5bf018989da96c4ca9
DajaT <dtrotter03@gmail.com>
2025-11-04 18:58:48 -0500

    Adding new files for Day 11 and moving to Week 2 folder

---
+++ Week_2/Day11_ES6_Features/day-11-es6-features.js
@@ -0,0 +1,34 @@
+//Arrow Functions
+const arrowLogin = (username, password) => {
+    return `Logging In ${username} with password ${password}`;
+}
+// Arrow function short hand
+const shortLogin = (username, password) => `Logging in ${username} with password ${password}`;
+
+//Template Literals
+const testUser ='john_doe';
+const testEnv ='staging';
+const testCount = 5;
+
+//Short Hand
+const newMessage = `Running ${testCount} tests for user ${testUser} in ${testEnv}`;
+
+//Deconstructing
+const testConfig = {
+    baseURL: 'https://testapp.com',
+    timeout: 30000,
+    retries: 3,
+    browser: 'chronium',
+}
+
+//Short Hand
+const {baseURL, timeout} = testConfig;
+console.log(baseURL, timeout);
+
+//Spread Operator (...)
+const basicTests =['login', 'logout'];
+const advancedTests =['api_test', 'performance_test'];
+
+//Short Hand
+const AllTests = [...basicTests, ...advancedTests, 'securityTests'];
+console.log(AllTests);
