f1378c21cf8ea88392b01516a4b0051dfaf04241
DajaT <dtrotter03@gmail.com>
2025-11-22 14:54:49 -0500

    Add Day 18 work and updated config file

--- Week-Three-Learning/tests/day17-waits-timeouts.spec.js
+++ Week-Three-Learning/tests/day17-waits-timeouts.spec.js
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('Day 17 - Waits and Timeouts', async ({ page }) => {
-console.log('Step 1: Navigate to the Mini-Bank Website');
+console.log('Step 1: Go to the Mini-Bank Website');
 await page.goto('https://mini-bank.testamplify.com/login');
 await expect(page).toHaveURL('https://mini-bank.testamplify.com/login');
 
-console.log('Step 2: Login to the account');
+console.log('Step 2: Login');
 const email = page.locator("//input[@placeholder='you@example.com']")
 await email.fill('testuser2@yopmail.com');
 
@@ -15,35 +15,35 @@ await password.fill('Pass2005#');
 const loginBtn = page.locator("//button[@type='submit']");
 await loginBtn.click();
 
-console.log('Step 3: Waiting For Dashboard to load');
+console.log('Step 3: Wait For Dashboard');
 await page.waitForLoadState("networkidle");
 await expect(page).toHaveURL('https://mini-bank.testamplify.com/dashboard');
 
-console.log('Step 4: Wait for transaction history to appear');
-await page.waitForSelector("//h2[contains(.,'Recent Transactions')]");
-const transactions = page.locator("//div[contains(@class,'transactions') or contains(.,'Deposit')]");
+console.log('Step 4: Wait for transaction history');
+await page.waitForSelector("//h2[contains(.,'Transactions')]");
+const transactions = page.locator("//div[contains(@class,'Withdrawals') or contains(.,'Deposit')]");
 await expect(transactions.first()).toBeVisible();
 
-console.log('STEP 5: Click "See All" to open All Transactions');
+console.log('STEP 5: Click "See All" to see all Transactions');
 const seeAllButton = page.locator("//a[contains(.,'See All')]");
 await seeAllButton.click();
 await page.waitForURL("**/dashboard/transactions");
 await expect(page).toHaveURL(/transactions/);
 
-console.log("STEP 6: Wait for transactions to load");
-await page.waitForSelector("//table[contains(.,'Personal') or //h2[text()='Withdrawal']]");
-const amount = page.locator("//td[contains(.,'$')]");
-await expect(amount.first()).toBeVisible(); //returns locator to first matching element
-await expect(amount.first()).toContainText('$');
+console.log("STEP 6: Wait for transactions");
+await page.waitForSelector("//table[contains(.,'General')]");
+const amountNo = page.locator("//td[contains(.,'$')]");
+await expect(amountNo.first()).toBeVisible(); //returns locator to first matching element
+await expect(amountNo.first()).toContainText('$');
 
-console.log("STEP 8: Click the Dashboard tab");
-const DashboardButton = page.locator("//span[text()='Dashboard']");
-await DashboardButton.click();
+console.log("STEP 8: Click the on Dashboard tab");
+const DashboardBtn = page.locator("//span[text()='Dashboard']");
+await DashboardBtn.click();
 
 console.log('STEP 9: Validate that "Start a transaction" button is enabled');
-const transactionButton = page.locator("//button[contains(.,'Start a transaction')]");
-await expect(transactionButton).toBeEnabled();
+const transactionBtn = page.locator("//button[contains(.,'Start a transaction')]");
+await expect(transactionBtn).toBeEnabled();
 
-console.log("✅ TEST COMPLETE: Page loaded properly and waits handled smoothly.");
+console.log("✅ TEST COMPLETE: Page loaded properly with waits.");
 
 });
---
+++ Week-Three-Learning/tests/day18-screenshots-videos.spec.js
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test';
+
+test('Day 17 - Waits and Timeouts', async ({ page }) => {
+console.log('Step 1: Go to the Mini-Bank Website');
+await page.setViewportSize({ width: 1920, height: 1080 });
+await page.goto('https://mini-bank.testamplify.com/login');
+await expect(page).toHaveURL('https://mini-bank.testamplify.com/login');
+
+console.log('Step 2: Login');
+const email = page.locator("//input[@placeholder='you@example.com']")
+await email.fill('testuser2@yopmail.com');
+
+const password = page.locator("//input[@placeholder='Enter your password']");
+await password.fill('Pass2005#');
+
+const loginBtn = page.locator("//button[@type='submit']");
+await loginBtn.click();
+
+console.log('Step 3: Wait For Dashboard');
+await page.waitForLoadState("networkidle");
+await expect(page).toHaveURL('https://mini-bank.testamplify.com/dashboard');
+
+console.log('Step 4: Navigate to Transactions')
+await page.waitForLoadState("networkidle");
+const TransactionBtn = page.locator("//a[@href='/dashboard/transaction']");
+await TransactionBtn.click();
+await expect(page).toHaveURL('https://mini-bank.testamplify.com/dashboard/transactions');
+
+console.log('Step 5: Take screenshot of Transactions');
+await page.screenshot({
+    path: 'test-results/day18-transaction-screenshot.png', fullPage: true, //manually taking a screenshot
+    path: 'test-results/day18-balances-screenshot.png', fullPage: true,
+})
+
+console.log('✅ Successful Transactions Page Screenshot');
+
+});
