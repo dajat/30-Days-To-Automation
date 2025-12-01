fcdfdc22b284132f6b229fc28b1539bc7c7a2d90
DajaT <dtrotter03@gmail.com>
2025-11-30 19:19:24 -0500

    Updating transfer page and test for Mini Bank project and adding Mini-Shop-Project

--- Mini-Bank-Project/pages/transfer.page.js
+++ Mini-Bank-Project/pages/transfer.page.js
@@ -1,15 +1,17 @@
 import { expect } from '@playwright/test';
-import { urls } from '../test-data/credentials';
+import { urls, creds } from '../test-data/credentials';
 
 export class TransferPage {
     constructor(page){
         this.page = page;
         this.transactionBtn = page.getByRole('button', {name: /Start a transaction/i });
-        this.transferBtn = page.locator('//a[@href="dashboard/transfer"]');
+        this.transferBtn = page.locator('//a[@href="/dashboard/transfer"]');
+        this.transferMoneyBtn = page.getByRole('link', { name: 'Transfer Money' });
     }
 
-    async assertLoaded() {
-    await expect(this.transactionBtn).toBeVisible();
-    await expect(this.transferBtn).toBeVisible();
-    }
+  async startTransfer() {
+    await this.transactionBtn.click();
+    await this.transferMoneyBtn.click();
+    await expect(this.page).toHaveURL(/dashboard\/transfer/);
 }
+    }
--- Mini-Bank-Project/tests/transfer.spec.js
+++ Mini-Bank-Project/tests/transfer.spec.js
@@ -1,19 +1,28 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { LandingPage } from '../pages/landing.page';
 import { LoginPage } from '../pages/login.page';
-import { TransferPage } from '..pages/transfer.page';
-import { urls } from '..test-data/credentials';
+import { TransferPage } from '../pages/transfer.page';
+import { urls, creds } from '../test-data/credentials';
 
-test(''), async ({ page }) => {
+test('Start A Transaction - Transfer Money', async ({ page }) => {
     const transfer = new TransferPage(page);
     const landing = new LandingPage (page);
     const login = new LoginPage (page);
+    console.log('✅ 1. Navigate to website');
 
     await landing.goto(urls.home);
     await landing.clickLogin();
     await login.assertLoaded();
+    console.log('✅ 2. Verify that the page is visible');
 
     await login.login(creds.valid.email, creds.valid.password);
-    await transactionBtn.click();
+    await expect(page).toHaveURL('https://mini-bank.testamplify.com/dashboard');
+    await transfer.transactionBtn.click();
+    await transfer.transferMoneyBtn.click();
+    await expect(page).toHaveURL('https://mini-bank.testamplify.com/dashboard/transfer');
+    console.log('✅ 3. Login to website and navigate to Transfer Page')
+
+    await page.screenshot({ path: 'Transfer-page.png', fullPage: true });
+    console.log('✅ 4. Screenshot of Transfer Page');
     
-}
+});
