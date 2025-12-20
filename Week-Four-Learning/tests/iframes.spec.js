1c7faf096309756c002c9b9d24993cf19e74d282
DajaT <dtrotter03@gmail.com>
2025-12-20 17:30:48 -0500

    adding iframe test and pom

---
+++ Week-Four-Learning/tests/iframes.spec.js
@@ -0,0 +1,9 @@
+import { test } from '@playwright/test';
+import { iframepage } from '../pages/iframepage.js';
+
+test('Iframe: type inside the editor', async ({ page }) => {
+  const iframe = new iframepage(page);
+  await iframe.goto();
+  await iframe.checkEditor();
+  await iframe.expectEditorContains('Your content goes here.');
+});
