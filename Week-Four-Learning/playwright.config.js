8e2d659b13c5615437e7bb0f08844daa62e0187f
DajaT <dtrotter03@gmail.com>
2025-12-23 19:29:08 -0500

    Updating all test data, test cases, and poms for bank smoke test

--- Week-Four-Learning/playwright.config.js
+++ Week-Four-Learning/playwright.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   // ✅ Always keep this simple
   testDir: './tests',
+
+   // 🕒 Global timeouts
+  timeout: 30_000,
+  expect: { timeout: 5_000 },
+
+  retries: 0,
+
+  // run tests in files in parallel
+   fullyParallel: true,
+
+   // let Playwright choose a good number of workers (parallel runs)
+   // (you can override on CLI with --workers=6, etc.)
+   workers: '100%',
 
   // ✅ Match your file naming
   testMatch: /.*\.spec\.js/,
@@ -20,9 +33,9 @@ export default defineConfig({
   reporter: [['html', { open: 'never' }]],
 
   projects: [
-    {
-      name: 'chromium',
-      use: { browserName: 'chromium' },
-    },
+    {name: 'chromium', use: { browserName: 'chromium' }, screenshot: 'only-on-failure',},
+    { name: 'firefox',  use: { ...devices['Desktop Firefox'], screenshot: 'only-on-failure', } },
+    //{ name: 'webkit',   use: { ...devices['Desktop Safari'] } },
+    {name: 'edge', use: { ...devices['Desktop Edge'], channel: 'msedge', screenshot: 'only-on-failure'}}, // runs real Microsoft Edge 
   ],
 });
